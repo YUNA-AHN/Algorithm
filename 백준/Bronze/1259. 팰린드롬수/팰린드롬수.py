0e6b6ca3666a0f6559f0c0b403ecb927f82153cc
AHN YUNA <yunanash1234@gmail.com>
2023-07-19 15:57:14 +0900

    Update 팰린드롬수.py

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
@@ -1,10 +1,15 @@
-palin = ''
-while palin != '0':
-    palin  = input()
-    if palin == '0':
-        break
-    else: 
-        if palin == palin[::-1]:
-            print('yes')
-        else:
-            print('no')
+# palin = ''
+# while palin != '0':
+#     palin  = input()
+#     if palin == '0':
+#         break
+#     else: 
+#         if palin == palin[::-1]:
+#             print('yes')
+#         else:
+#             print('no')
+
+while True :
+    n = input()
+    if n == "0" : break
+    print('yes' if n == n[::-1] else 'no')
