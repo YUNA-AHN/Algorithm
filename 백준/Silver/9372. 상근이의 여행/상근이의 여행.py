59b09a660f712f2a8df35cdd294f501c68727b0b
AHN YUNA <yunanash1234@gmail.com>
2023-09-18 20:41:53 +0900

    [Silver IV] Title: 상근이의 여행, Time: 260 ms, Memory: 139220 KB -BaekjoonHub

--- 백준/Silver/9372. 상근이의 여행/상근이의 여행.py
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.py
@@ -3,15 +3,9 @@ def tree(u):
     cnt : 방문한 국가 수
     - 이미 방문한 적이 있다면 카운팅 x
     - 방분한 국가 수가 n과 동일해지면 종료
-    :return:
     '''
     global cnt, mn
     if cnt == n:
-        # 최솟값 구하기
-        mn = min(cnt, mn)
-        cnt = 0
-        return
-    if cnt > mn:
         return
     for i in arr[u]:
         if visited[i] == 0:
@@ -25,6 +19,7 @@ input = sys.stdin.readline
 t = int(input().strip())
 for tc in range(t):
     n, m = map(int, input().strip().split())
+    # 각 노드에서 방문할 수 있는 방문 리스트 생성
     arr = [[] for _ in range(n+1)]
     visited = [0] * (n+1)
 
@@ -36,4 +31,4 @@ for tc in range(t):
     cnt = 0
     mn = 10 ** 7
     tree(1)
-    print(mn-1)
+    print(cnt-1)
