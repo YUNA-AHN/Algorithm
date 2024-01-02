5c4bcd77f4a7e17f5b7f9b2642a2ba2126e732b1
AHN YUNA <yunanash1234@gmail.com>
2024-01-03 03:50:37 +0900

    [level 2] Title: 무인도 여행, Time: 0.20 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.py
@@ -0,0 +1,27 @@
+def solution(maps):
+    from collections import deque
+    import heapq
+    answer = []
+    li = len(maps)
+    lj = len(maps[0])
+    visited = [[0] * lj for _ in range(li)]
+    for i in range(li):
+        for j in range(lj):
+            if maps[i][j] != "X" and visited[i][j] == 0:
+                print(i, j)
+                que = deque()
+                que.append((i, j))
+                visited[i][j] = 1
+                meal = int(maps[i][j])
+                while que:
+                    x, y = que.popleft()
+                    for dx, dy in [[0,1],[0,-1],[1,0],[-1,0]]:
+                        nx, ny = x + dx, y + dy
+                        if 0 <= nx < li and 0 <= ny < lj and maps[nx][ny] != "X" and visited[nx][ny] == 0:
+                            que.append((nx, ny))
+                            visited[nx][ny] = 1
+                            meal += int(maps[nx][ny])
+                heapq.heappush(answer, meal)
+    if not answer:
+        answer.append(-1)
+    return sorted(answer)
