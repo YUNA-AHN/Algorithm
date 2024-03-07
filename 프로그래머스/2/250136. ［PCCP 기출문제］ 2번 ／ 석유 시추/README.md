2f67526a60843ceafcc2c1a1eba97443b0dc196e
AHN YUNA <yunanash1234@gmail.com>
2024-03-07 20:31:00 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 56.39 ms, Memory: 13.7 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/README.md
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/README.md
@@ -1,10 +1,10 @@
 # [level 2] [PCCP 기출문제] 2번 / 석유 시추 - 250136 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/250136?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/250136) 
 
 ### 성능 요약
 
-메모리: 13.7 MB, 시간: 56.34 ms
+메모리: 13.7 MB, 시간: 56.39 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 07일 19:44:42
+2024년 03월 07일 20:30:59
 
 ### 문제 설명
 
--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.py
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.py
@@ -2,11 +2,12 @@ def solution(land):
     from pprint import pprint
     from collections import deque
     answer = 0
-    dump =  10
-    oil = 0
-    oil_lst = []
+    dump =  10 # 석유 덩어리 번호
+    oil = 0 # 석유 양
+    oil_lst = [] # 석유 양 리스트
     r, c = len(land), len(land[0])
     visited = [[0] * c for _ in range(r)]
+    # 순회하면서 1인 경우 bfs 진행
     for i in range(r):
         for j in range(c):
             if land[i][j] == 1 and visited[i][j] == 0:
@@ -14,7 +15,7 @@ def solution(land):
                 que.append((i, j))
                 land[i][j] = dump
                 visited[i][j] = 1
-                oil += 1
+                oil += 1 # 석유 양 더하기
                 while que:
                     x, y = que.popleft()
                     for a, b in [[0,1], [0,-1], [-1,0], [1,0]]:
@@ -24,13 +25,15 @@ def solution(land):
                             visited[nx][ny] = 1
                             oil += 1
                             land[nx][ny] = dump
-                dump += 1
-                oil_lst.append(oil)
-                oil = 0
+                dump += 1 # 인덱스 번호 + 1
+                oil_lst.append(oil) # 석유 양 리스트
+                oil = 0 # 석유 양 초기화
+    # 열 별로 set 하여 양 더하기
     for col in zip(*land):
         total = 0
         for idx in list(set(col)):
             if idx != 0:
                 total += oil_lst[idx-10]
+        # 석유 양이 가장 많은 경우
         answer = max(total, answer)
     return answer
