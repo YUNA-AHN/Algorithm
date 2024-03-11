7238e79dd266c6bd10825486db56239eac2db380
AHN YUNA <yunanash1234@gmail.com>
2024-03-11 22:59:23 +0900

    [level 1] Title: Python 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
@@ -0,0 +1,6 @@
+-- Python 스킬을 가진 개발자 정보 조회
+-- ID, 이메일, 이름, 성 조회 : ID를 기준으로 오름차순 정렬
+SELECT ID, EMAIL, FIRST_NAME, LAST_NAME
+FROM DEVELOPER_INFOS
+WHERE SKILL_1 = 'Python' OR SKILL_2 = 'Python' OR SKILL_3 = 'Python'
+ORDER BY ID;
