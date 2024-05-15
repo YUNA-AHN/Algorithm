ff84639191aaceffd7776f9b0949778a779c09f9
AHN YUNA <yunanash1234@gmail.com>
2024-05-16 02:01:57 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.12 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -0,0 +1,19 @@
+import java.util.Stack;
+class Solution {
+    public int solution(int[] ingredient) {
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < ingredient.length; i++) {
+            stack.push(ingredient[i]);
+            
+            if (stack.size() >= 4 && stack.peek() == 1 && stack.get(stack.size()-2) == 3 && stack.get(stack.size() - 3) == 2 && stack.get(stack.size() - 4) == 1) {
+                stack.pop();
+                stack.pop();
+                stack.pop();
+                stack.pop();
+                answer += 1;
+            }
+        }
+        return answer;
+    }
+}
