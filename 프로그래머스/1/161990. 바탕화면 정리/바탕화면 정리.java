48173683e9f63e05a5d2f4764f7ed0882d1a3e00
AHN YUNA <yunanash1234@gmail.com>
2024-05-21 20:44:47 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.15 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -2,20 +2,14 @@ class Solution {
     public int[] solution(String[] wallpaper) {
         int r = wallpaper.length;
         int c = wallpaper[0].length();
-        int cnt = 0, lux = 0, luy = 0, rdx = 0, rdy = 0;
+        int cnt = 0, lux = r, luy = c, rdx = 0, rdy = 0;
         for (int i = 0; i < r; i++){
             for (int j = 0; j < c; j++){
                 if (wallpaper[i].charAt(j) == '#'){
-                    if (cnt == 0){
-                        lux = i; luy = j; rdx = i + 1; rdy = j + 1; 
-                        cnt += 1;
-                    }
-                    else {
-                        lux = (lux < i) ? lux : i;
-                        luy = (luy < j) ? luy : j;
-                        rdx = (rdx > i) ? rdx : i + 1;
-                        rdy = (rdy > j) ? rdy : j + 1;
-                    }
+                    lux = (lux < i) ? lux : i;
+                    luy = (luy < j) ? luy : j;
+                    rdx = (rdx > i) ? rdx : i + 1;
+                    rdy = (rdy > j) ? rdy : j + 1;
                 }
             }
         }
