aaedb8edc3f39ab2a517299a79633a21fccfed08
AHN YUNA <yunanash1234@gmail.com>
2024-12-16 03:53:24 +0900

    [level 2] Title: 카펫, Time: 0.06 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,8 +1,6 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
-        // 행이 1줄 늘때마다 brown 2개 증가
-        // (brown - 2 * (1)) / 2 이 값에서 시작해서 3까지
         for (int c = (brown - 2) / 2; c >= 3; c--){
             int r = (brown - 2 * (c)) / 2 + 2;
             if ((c - 2) * (r - 2) == yellow){
